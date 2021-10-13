3826dd8cec3c5fb95b35beb45d24b90b91992899
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-10-13 15:57:22 +0200

    Mis à jour de la présence des fichiers dans le projet

--- SPYROpenGL/spyropengl.cpp
+++ SPYROpenGL/spyropengl.cpp
@@ -13,37 +13,9 @@
 #endif
 #include <cstdlib>
 
+#include "Actions.h"
 
-//Tableau pour stocker les sommets du cube et leur couleur
-Point pCube[8]={
-    {-0.5,-0.5, 0.5,1.0,0.0,0.0},
-    { 0.5, -0.5, 0.5,0.0,1.0,0.0},
-    { 0.5, -0.5, -0.5,0.0,0.0,1.0},
-    { -0.5, -0.5, -0.5,1.0,1.0,1.0},
-    { -0.5,0.5, 0.5,1.0,0.0,0.0},
-    { 0.5, 0.5, 0.5,0.0,1.0,0.0},
-    { 0.5, 0.5, -0.5,0.0,0.0,1.0},
-    { -0.5, 0.5, -0.5,1.0,1.0,1.0}};
-
-//Tableau pour stocker les indices des sommets par face pour le cube
-int fCube[6][4]={
-  {0,3,2,1},
-  {0,1,5,4},
-  {1,2,6,5},
-  {2,3,7,6},
-  {0,4,7,3},
-  {4,5,6,7}};
-
-char presse;
-int anglex,angley,x,y,xold,yold;
-
-/* Prototype des fonctions */
 void affichage();
-void clavier(unsigned char touche,int x,int y);
-void reshape(int x,int y);
-void idle();
-void mouse(int bouton,int etat,int x,int y);
-void mousemotion(int x,int y);
 
 int main(int argc,char **argv)
 {
@@ -62,39 +34,35 @@ int main(int argc,char **argv)
   glEnable(GL_DEPTH_TEST);
 
   /* enregistrement des fonctions de rappel */
+  Actions a = Actions();
   glutDisplayFunc(affichage);
-  glutKeyboardFunc(clavier);
-  glutReshapeFunc(reshape);
-  glutMouseFunc(mouse);
-  glutMotionFunc(mousemotion);
+  glutKeyboardFunc(a.clavier);
+  glutReshapeFunc(a.reshape);
+  glutMouseFunc(a.mouse);
+  glutMotionFunc(a.mousemotion);
 
   /* Entree dans la boucle principale glut */
   glutMainLoop();
   return 0;
 }
 
-
 void affichage()
 {
-  int i,j;
-  /* effacement de l'image avec la couleur de fond */
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-glShadeModel(GL_SMOOTH);
+    int i,j;
+    /* effacement de l'image avec la couleur de fond */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glShadeModel(GL_SMOOTH);
 
-  glLoadIdentity();
-  glRotatef(angley,1.0,0.0,0.0);
-  glRotatef(anglex,0.0,1.0,0.0);
+    glLoadIdentity();
+    glRotatef(angley,1.0,0.0,0.0);
+    glRotatef(anglex,0.0,1.0,0.0);
 
-   // Dessin du cube
-  for (i=0;i<6;i++)
-    {
-      glBegin(GL_POLYGON);
-      for (j=0;j<4;j++){
-          glColor3f(pCube[fCube[i][j]].r,pCube[fCube[i][j]].g,pCube[fCube[i][j]].b);
-          glVertex3f(pCube[fCube[i][j]].x,pCube[fCube[i][j]].y,pCube[fCube[i][j]].z);
-      }
-      glEnd();
-    }
+   //Dessin de la base parallélépipède
+    glPushMatrix();
+    glColor3f(0.2, 0.4, 0.698039215686);
+    glScalef(2,0.5,1);
+    glutSolidCube(1);
+    glPopMatrix();
 
     //Repère
     //axe x en rouge
@@ -121,70 +89,3 @@ glShadeModel(GL_SMOOTH);
   //On echange les buffers
   glutSwapBuffers();
 }
-
-void clavier(unsigned char touche,int x,int y)
-{
-  switch (touche)
-    {
-    case 'p': /* affichage du carre plein */
-      glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-      glutPostRedisplay();
-      break;
-    case 'f': /* affichage en mode fil de fer */
-      glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-      glutPostRedisplay();
-      break;
-    case 's' : /* Affichage en mode sommets seuls */
-      glPolygonMode(GL_FRONT_AND_BACK,GL_POINT);
-      glutPostRedisplay();
-      break;
-    case 'd':
-      glEnable(GL_DEPTH_TEST);
-      glutPostRedisplay();
-      break;
-    case 'D':
-      glDisable(GL_DEPTH_TEST);
-      glutPostRedisplay();
-      break;
-    case 'q' : /*la touche 'q' permet de quitter le programme */
-      exit(0);
-    }
-}
-
-void reshape(int x,int y)
-{
-  if (x<y)
-    glViewport(0,(y-x)/2,x,x);
-  else
-    glViewport((x-y)/2,0,y,y);
-}
-
-void mouse(int button, int state,int x,int y)
-{
-  /* si on appuie sur le bouton gauche */
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-  {
-    presse = 1; /* le booleen presse passe a 1 (vrai) */
-    xold = x; /* on sauvegarde la position de la souris */
-    yold=y;
-  }
-  /* si on relache le bouton gauche */
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
-    presse=0; /* le booleen presse passe a 0 (faux) */
-}
-
-void mousemotion(int x,int y)
-  {
-    if (presse) /* si le bouton gauche est presse */
-    {
-      /* on modifie les angles de rotation de l'objet
-	 en fonction de la position actuelle de la souris et de la derniere
-	 position sauvegardee */
-      anglex=anglex+(x-xold);
-      angley=angley+(y-yold);
-      glutPostRedisplay(); /* on demande un rafraichissement de l'affichage */
-    }
-
-    xold=x; /* sauvegarde des valeurs courante de le position de la souris */
-    yold=y;
-  }
