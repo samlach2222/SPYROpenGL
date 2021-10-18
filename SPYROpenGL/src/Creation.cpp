10185409cf1c4ee61d53a7fb30451fdbdc4ca84c
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-10-18 22:06:41 +0200

    Correction création corps

--- SPYROpenGL/src/Creation.cpp
+++ SPYROpenGL/src/Creation.cpp
@@ -202,7 +202,7 @@ const void Creation::Aile(float ecart, float largeur, float longueur, float haut
 
 const void Creation::Corps(int NM, float rayon, float longueur){
     glPushMatrix();
-        //glRotatef(90,1,0,0);
+        glRotatef(90,1,0,0);
         glRotatef((3*M_PI/10)*(180/M_PI), 0, 1, 0);  //angle en radians * 180/pi --> angle en degrés
         Dessin::Cylindre(NM, rayon, longueur);
     glPopMatrix();
